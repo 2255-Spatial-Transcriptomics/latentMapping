2b1a406bd868ccca23b777becd9c960cb0ee94e1
zsayem30 <sayemnazmuz@gmail.com>
2023-04-02 10:42:41 +0000

    Modified test_step_1.py

--- test_step_1.py
+++ test_step_1.py
@@ -22,41 +22,7 @@ warnings.filterwarnings("ignore")
 torch.manual_seed(2)
 np.random.seed(2)
 
-# sc_adata = loadSCDataset()
-# st_adata = loadSTDataset() 
-
-# x_adata = sampleHighestExpressions(st_adata)
-
-# # find the common genes between the two datasets
-# common_genes = findCommonGenes(st_adata, sc_adata)
-
-# # extract only expression data from sc dataset that share the common genes
-# xprime_adata = extractGenes(sc_adata, common_genes)
-
-# # extract only expreesion data from st dataset that share the common genes
-# xbar_adata = extractGenes(st_adata, common_genes)
-
-adata_sc_read = sc.read_h5ad("data/st_top5k_genes.h5ad")
-
-adata_sc_read.layers["counts"] = adata_sc_read.X.copy()  # preserve counts
-sc.pp.normalize_total(adata_sc_read, target_sum=1e4)
-sc.pp.log1p(adata_sc_read)
-adata_sc_read.raw = adata_sc_read  # freeze the state in `.raw`
-
-sc.pp.filter_genes(adata_sc_read, min_counts=3)
-sc.pp.filter_cells(adata_sc_read, min_counts=3)
-
-sc.pp.highly_variable_genes(
-    adata_sc_read,
-    n_top_genes=2000,
-    subset=True,
-    layer="counts",
-    flavor="seurat_v3"
-)
-
-sc.pp.filter_genes(adata_sc_read, min_counts=3)
-sc.pp.filter_cells(adata_sc_read, min_counts=3)
-
+adata_sc_read = sc.read_h5ad("/content/drive/MyDrive/Capstone scRNA dataset/scRNA_top2k_genes_new.h5ad")
 
 LATENT_DIM = 10
 # step 1
