3f3b63e107aa7f4e9f6a6355ae3b838fbc1a3af5
zsayem30 <sayemnazmuz@gmail.com>
2023-08-14 20:52:18 +0600

    Modified training plan. Still need to test it.

--- models/scviModels/trainingPlans.py
+++ models/scviModels/trainingPlans.py
@@ -1,9 +1,19 @@
 from typing import Optional, Union
 
 import numpy as np
-
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
 from scvi.dataloaders import DataSplitter
 from scvi.train import TrainingPlan, TrainRunner
+from scvi import REGISTRY_KEYS
+from scvi.module.base import BaseModuleClass
+from scvi.autotune._types import Tunable, TunableMixin
+from typing import Any, Callable, Dict, Iterable, Literal, Optional, Union
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+TorchOptimizerCreator = Callable[[Iterable[torch.Tensor]], torch.optim.Optimizer]
+
 
 class baseVAETrainingPlan(TrainingPlan):
     def __init__(self, module, **kwargs):
@@ -119,6 +129,227 @@ class scVAEUnsupervisedTrainingMixin:
 
     _data_splitter_cls = DataSplitter
     _training_plan_cls = scVAETrainingPlan
+    _train_runner_cls = TrainRunner
+
+    def train(
+        self,
+        max_epochs: Optional[int] = None,
+        use_gpu: Optional[Union[str, int, bool]] = None,
+        train_size: float = 0.9,
+        validation_size: Optional[float] = None,
+        batch_size: int = 128,
+        early_stopping: bool = False,
+        plan_kwargs: Optional[dict] = None,
+        **trainer_kwargs,
+    ):
+        """
+        Train the model.
+
+        Parameters
+        ----------
+        max_epochs
+            Number of passes through the dataset. If `None`, defaults to
+            `np.min([round((20000 / n_cells) * 400), 400])`
+        use_gpu
+            Use default GPU if available (if None or True), or index of GPU to use (if int),
+            or name of GPU (if str, e.g., `'cuda:0'`), or use CPU (if False).
+        train_size
+            Size of training set in the range [0.0, 1.0].
+        validation_size
+            Size of the test set. If `None`, defaults to 1 - `train_size`. If
+            `train_size + validation_size < 1`, the remaining cells belong to a test set.
+        batch_size
+            Minibatch size to use during training.
+        early_stopping
+            Perform early stopping. Additional arguments can be passed in `**kwargs`.
+            See :class:`~scvi.train.Trainer` for further options.
+        plan_kwargs
+            Keyword args for :class:`~scvi.train.TrainingPlan`. Keyword arguments passed to
+            `train()` will overwrite values present in `plan_kwargs`, when appropriate.
+        **trainer_kwargs
+            Other keyword args for :class:`~scvi.train.Trainer`.
+        """
+        n_cells = self.adata.n_obs
+        if max_epochs is None:
+            max_epochs = int(np.min([round((20000 / n_cells) * 400), 400]))
+
+        plan_kwargs = plan_kwargs if isinstance(plan_kwargs, dict) else dict()
+
+        data_splitter = self._data_splitter_cls(
+            self.adata_manager,
+            train_size=train_size,
+            validation_size=validation_size,
+            batch_size=batch_size,
+            use_gpu=use_gpu,
+        )
+        training_plan = self._training_plan_cls(self.module, **plan_kwargs)
+
+        es = "early_stopping"
+        trainer_kwargs[es] = (
+            early_stopping if es not in trainer_kwargs.keys() else trainer_kwargs[es]
+        )
+        runner = self._train_runner_cls(
+            self,
+            training_plan=training_plan,
+            data_splitter=data_splitter,
+            max_epochs=max_epochs,
+            use_gpu=use_gpu,
+            **trainer_kwargs,
+        )
+        return runner()
+## Added section
+class scVAE2TrainingPlan(TrainingPlan):
+
+    """Training plan for vae2 (scvi). Code is modified from Adversarial Traning Plan: 
+    https://github.com/scverse/scvi-tools/blob/1.0.3/scvi/train/_trainingplans.py#L21"""
+    def __init__(
+        self,
+        module: BaseModuleClass,
+        *,
+        optimizer: Tunable[Literal["Adam", "AdamW", "Custom"]] = "Adam",
+        optimizer_creator: Optional[TorchOptimizerCreator] = None,
+        lr: Tunable[float] = 1e-3,
+        weight_decay: Tunable[float] = 1e-6,
+        n_steps_kl_warmup: Tunable[int] = None,
+        n_epochs_kl_warmup: Tunable[int] = 400,
+        reduce_lr_on_plateau: Tunable[bool] = False,
+        lr_factor: Tunable[float] = 0.6,
+        lr_patience: Tunable[int] = 30,
+        lr_threshold: Tunable[float] = 0.0,
+        lr_scheduler_metric: Literal[
+            "elbo_validation", "reconstruction_loss_validation", "kl_local_validation"
+        ] = "elbo_validation",
+        lr_min: float = 0,
+        scale_euclidean_loss: Union[float, Literal["auto"]] = "auto",
+        **loss_kwargs,
+    ):
+        super().__init__(
+            module=module,
+            optimizer=optimizer,
+            optimizer_creator=optimizer_creator,
+            lr=lr,
+            weight_decay=weight_decay,
+            n_steps_kl_warmup=n_steps_kl_warmup,
+            n_epochs_kl_warmup=n_epochs_kl_warmup,
+            reduce_lr_on_plateau=reduce_lr_on_plateau,
+            lr_factor=lr_factor,
+            lr_patience=lr_patience,
+            lr_threshold=lr_threshold,
+            lr_scheduler_metric=lr_scheduler_metric,
+            lr_min=lr_min,
+            **loss_kwargs,
+        )
+
+    def loss_euclidean_map(self, z, z_prime):
+        """ euclidean mapping loss between batch and latent space of x"""
+        loss_func = nn.MSELoss()
+        similarity_loss = loss_func(torch.tensor(z), z_prime)
+        
+        return similarity_loss
+    
+    def training_step(self, batch, batch_idx):
+        """Training step for the model."""
+        if "kl_weight" in self.loss_kwargs:
+            kl_weight = self.kl_weight
+            self.loss_kwargs.update({"kl_weight": kl_weight})
+        
+        kappa = self.scale_euclidean_loss
+
+        z = self.datasets['z']
+
+        batch_tensor = batch[REGISTRY_KEYS.BATCH_KEY]
+
+        opts = self.optimizers()
+        opt1, opt2 = opts
+
+        inference_outputs, _, scvi_loss = self.forward(
+            batch, loss_kwargs=self.loss_kwargs
+        )
+
+        z_prime = inference_outputs["z"]
+        fool_loss = self.loss_euclidean_map(z, z_prime)
+        loss += fool_loss * kappa
+
+        self.log("train_loss", loss, on_epoch=True, prog_bar=True)
+        self.compute_and_log_metrics(scvi_loss, self.train_metrics, "train")
+        opt1.zero_grad()
+        self.manual_backward(loss)
+        opt1.step()
+
+        loss = self.loss_euclidean_map(z, z_prime)
+        loss *= kappa
+        opt2.zero_grad()
+        self.manual_backward(loss)
+        opt2.step()
+
+    def on_train_epoch_end(self):
+        """Update the learning rate via scheduler steps."""
+        if "validation" in self.lr_scheduler_metric or not self.reduce_lr_on_plateau:
+            return
+        else:
+            sch = self.lr_schedulers()
+            sch.step(self.trainer.callback_metrics[self.lr_scheduler_metric])
+
+    def on_validation_epoch_end(self) -> None:
+        """Update the learning rate via scheduler steps."""
+        if (
+            not self.reduce_lr_on_plateau
+            or "validation" not in self.lr_scheduler_metric
+        ):
+            return
+        else:
+            sch = self.lr_schedulers()
+            sch.step(self.trainer.callback_metrics[self.lr_scheduler_metric])
+
+
+    def configure_optimizers(self):
+        """Configure optimizers for adversarial training."""
+        params1 = filter(lambda p: p.requires_grad, self.module.parameters())
+        optimizer1 = self.get_optimizer_creator()(params1)
+        config1 = {"optimizer": optimizer1}
+        if self.reduce_lr_on_plateau:
+            scheduler1 = ReduceLROnPlateau(
+                optimizer1,
+                patience=self.lr_patience,
+                factor=self.lr_factor,
+                threshold=self.lr_threshold,
+                min_lr=self.lr_min,
+                threshold_mode="abs",
+                verbose=True,
+            )
+            config1.update(
+                {
+                    "lr_scheduler": {
+                        "scheduler": scheduler1,
+                        "monitor": self.lr_scheduler_metric,
+                    },
+                },
+            )
+
+        if self.adversarial_classifier is not False:
+            params2 = filter(
+                lambda p: p.requires_grad, self.adversarial_classifier.parameters()
+            )
+            optimizer2 = torch.optim.Adam(
+                params2, lr=1e-3, eps=0.01, weight_decay=self.weight_decay
+            )
+            config2 = {"optimizer": optimizer2}
+
+            # pytorch lightning requires this way to return
+            opts = [config1.pop("optimizer"), config2["optimizer"]]
+            if "lr_scheduler" in config1:
+                scheds = [config1["lr_scheduler"]]
+                return opts, scheds
+            else:
+                return opts
+
+        return config1
+
+class scVAE2UnsupervisedTrainingMixin:
+    """Training method for VAE2 (from scvi) """
+
+    _data_splitter_cls = DataSplitter
+    _training_plan_cls = scVAE2TrainingPlan
     _train_runner_cls = TrainRunner
 
     def train(
